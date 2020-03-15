1690cabab8f6b4284a893139382eff988db1e0a6
rdbbl <formecc@gmail.com>
2020-03-15 13:13:21 +0300

    .

--- source/js/slider.js
+++ source/js/slider.js
@@ -1,1 +1,11 @@
-
+var mySwiper = new Swiper ('.slider__container', {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 'auto',
+  centeredSlides: true,
+  spaceBetween: 60,
+  // If we need pagination
+  pagination: {
+    el: '.slider__pagination',
+  }
+});
