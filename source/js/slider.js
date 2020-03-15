c00c88c5981aa9493c3e3719f3abf9e1363956d4
Vitaly Mes <volkondr@ya.ru>
2020-03-15 19:13:48 +0300

    .

--- source/js/slider.js
+++ source/js/slider.js
@@ -1,11 +1,16 @@
 var mySwiper = new Swiper ('.slider__container', {
-  // Optional parameters
   loop: true,
   slidesPerView: 'auto',
   centeredSlides: true,
   spaceBetween: 60,
-  // If we need pagination
+  navigation: {
+    nextEl: '.slider__button--next',
+    prevEl: '.slider__button--prev',
+  },
   pagination: {
     el: '.slider__pagination',
+    clickable: true
   }
 });
+
+
